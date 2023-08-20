0b36d6682d0d4740c909ef0263b4b8a9aa45c7d6
cukminseo <kms39273@naver.com>
2023-08-20 18:33:33 +0900

    [FEAT] 하단 navbar 구현

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import BottomNav from "./pages/customer/BottomNav.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <BottomNav />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
