d70be9034ba61c8592f790c328d84880c81f75ee
cukminseo <kms39273@naver.com>
2023-08-20 20:27:39 +0900

    [FEAT] 네이버맵 기본 세팅

--- src/pages/customer/main/customerMain.tsx
+++ src/pages/customer/main/customerMain.tsx
@@ -1,7 +1,71 @@
+import { Container } from "../../../components/Container";
+import { useEffect, useState } from "react";
+// import axios, { AxiosError } from "axios";
+// import { atom, useRecoilState } from "recoil";
+import BottomNav from "../../../pages/customer/BottomNav.jsx";
 const CustomerMain = () => {
+  // const [storeList, setStoreList] = useRecoilState(storeListState);
+  // const [markers, setMarkers] = useRecoilState(markersState);
+  //네이버맵
+  useEffect(() => {
+    const mapDiv = document.getElementById("map");
+    const map = new window.naver.maps.Map(mapDiv);
+  }, []);
+  //검색창
+  const [searchWord, setSearchWord] = useState("");
+  //검색
+  // const searchStore = async () => {
+  //   try {
+  //     // 더미 데이터
+  //     const response = [
+  //       {
+  //         restaurant_id: 1,
+  //         restaurant_name: "서칭1",
+  //         latitude: 37.497175,
+  //         longitude: 127.027299,
+  //       },
+  //       {
+  //         restaurant_id: 2,
+  //         restaurant_name: "서칭2",
+  //         latitude: 37.497175,
+  //         longitude: 127.027926,
+  //       },
+  //     ];
+
+  //     setStoreList(response);
+  //     console.log("~~~~~~searchStoreList!!!", storeList);
+
+  //     const searchData = storeList.map((store: any) => {
+  //       return {
+  //         id: store.restaurant_id,
+  //         title: store.restaurant_name,
+  //         coordinate: {
+  //           latitude: store.latitude,
+  //           longitude: store.longitude,
+  //         },
+  //       };
+  //     });
+  //     console.log("~~~~~~ searchData입니다", searchData);
+  //     setMarkers(searchData);
+  //   } catch (error) {
+  //     console.error(error);
+  //     if ((error as AxiosError).response?.data.code === "400") {
+  //       alert(
+  //         "가게 정보를 불러오지 못했습니다. 통신 상태 확인 후 다시 한번 시도해주세요.",
+  //       );
+  //     }
+  //   }
+  // };
+
   return (
-    <div>
-      <h1>Customer Main</h1>
+    <div css={Container}>
+      <BottomNav />
+      <input
+        placeholder="검색할 내용을 입력해 주세요."
+        onChange={(e) => setSearchWord(e.target.value)}
+      />
+
+      <div id="map" style={{ height: "400px" }} />
     </div>
   );
 };
